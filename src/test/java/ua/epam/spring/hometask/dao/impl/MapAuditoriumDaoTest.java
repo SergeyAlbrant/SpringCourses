db4548e358c42af2a2f0b08985908b45dc7c9fed
SergeyAlbrant <albrants95@mail.ru>
2018-05-03 12:34:56 +0300

    test with xml works

--- src/test/java/ua/epam/spring/hometask/dao/impl/MapAuditoriumDaoTest.java
+++ src/test/java/ua/epam/spring/hometask/dao/impl/MapAuditoriumDaoTest.java
@@ -3,6 +3,7 @@ package ua.epam.spring.hometask.dao.impl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,15 +19,14 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring.xml"})
+@ContextConfiguration(locations = {"classpath:test.xml"})
 public class MapAuditoriumDaoTest {
 
+    @Autowired
     private MapAuditoriumDao auditoriumDao;
 
     @Before
     public void setUp() {
-        auditoriumDao = new MapAuditoriumDao();
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     }
 
     @Test
