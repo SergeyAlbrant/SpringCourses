5eea57c80fbab3d8e783e5f775f86ee02a4da73f
SergeyAlbrant <albrants95@mail.ru>
2018-05-05 13:50:44 +0300

    made test for booking service

--- src/test/java/com/epam/spring/hometask/service/BookingServiceTest.java
+++ src/test/java/com/epam/spring/hometask/service/BookingServiceTest.java
@@ -1,20 +1,45 @@
 package com.epam.spring.hometask.service;
 
+import com.epam.spring.hometask.dao.AuditoriumDao;
+import com.epam.spring.hometask.domain.Event;
+import com.epam.spring.hometask.domain.EventRating;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:test.xml"})
 public class BookingServiceTest {
+
+    @Autowired
+    private AuditoriumDao auditoriumDao;
+    @Autowired
+    private BookingService bookingService;
+
+    private Event event;
+    private static final LocalDateTime NOW = LocalDateTime.now();
+
+    @Before
+    public void setUp() {
+        event = new Event();
+        event.setBasePrice(20);
+        event.setRating(EventRating.HIGH);
+        event.addAirDateTime(NOW, auditoriumDao.getByName("VIP"));
+    }
+
 
     @Test
     public void getTicketsPrice() {
-    }
-
-    @Test
-    public void bookTickets() {
-    }
-
-    @Test
-    public void getPurchasedTicketsForEvent() {
+        double ticketsPrice = bookingService.getTicketsPrice(event, NOW, null, new HashSet<>(Arrays.asList(1L, 2L)));
+        assertTrue(ticketsPrice > 0);
     }
 }
